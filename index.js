4bcbdd27249ddeda5e3a3d8640fe2e16eb646c81
Creighton Medley <creighton.medley@gmail.com>
2020-01-24 20:11:19 -0500

    mount volume using as user syntax for Catalina

--- index.js
+++ index.js
@@ -1,47 +1,44 @@
-"use strict"
-const exec = require('child-process-promise').exec
+"use strict";
+const exec = require("child-process-promise").exec;
 const MAX_BUFFER_SIZE = 2000 * 1024;
-const applescript = require("applescript")
+const applescript = require("applescript");
 const macNetworkDrive = {
-
   list: function list() {
-
     let fetchDisks = exec("/bin/df", {
       maxBuffer: MAX_BUFFER_SIZE
     });
 
     let completePromise = new Promise((resolve, reject) => {
-      fetchDisks.then(result => {
-        if (typeof result.stderr === "string" && result.stderr.length !== 0) {
-          reject(stderr)
-        }
-
-        let pathList = result.stdout.split(/\s*[\n\r]+/g);
-        let drivePath = {}
-
-        pathList.splice(0, 1)
-        let regex = /^\/\/\S*@(\S*)\s*\S*\s*\S*\s*\S*\s*\S*\s*\S*\s*\S*\s*\S*\s*(\S*)$/
-        let re = new RegExp(regex, 'i')
-
-        for (let line of pathList) {
-          let matches = line.match(re);
-          if (matches) {
-            drivePath["\\\\" + matches[1].replace('/', '\\') + "\\"] = matches[2];
+      fetchDisks
+        .then(result => {
+          if (typeof result.stderr === "string" && result.stderr.length !== 0) {
+            reject(stderr);
           }
-        }
-        resolve(drivePath)
-      }).catch(err => {
 
-        reject(err)
+          let pathList = result.stdout.split(/\s*[\n\r]+/g);
+          let drivePath = {};
 
-      })
+          pathList.splice(0, 1);
+          let regex = /^\/\/\S*@(\S*)\s*\S*\s*\S*\s*\S*\s*\S*\s*\S*\s*\S*\s*\S*\s*(\S*)$/;
+          let re = new RegExp(regex, "i");
 
-    })
+          for (let line of pathList) {
+            let matches = line.match(re);
+            if (matches) {
+              drivePath["\\\\" + matches[1].replace("/", "\\") + "\\"] =
+                matches[2];
+            }
+          }
+          resolve(drivePath);
+        })
+        .catch(err => {
+          reject(err);
+        });
+    });
     return completePromise;
   },
   find: function find(drivePath) {
     let completePromise = new Promise((resolve, reject) => {
-
       if ("string" !== typeof drivePath || 0 === drivePath.length) {
         reject("Invalid path");
       }
@@ -52,70 +49,68 @@ const macNetworkDrive = {
             continue;
           }
 
-          if (drivePath.charAt(drivePath.length - 1) != "\\")
-            drivePath += "\\"
+          if (drivePath.charAt(drivePath.length - 1) != "\\") drivePath += "\\";
 
           if (currentDrivePath.toUpperCase() === drivePath.toUpperCase()) {
             resolve(networkDrives[currentDrivePath]);
           }
         }
         resolve(undefined);
-      })
-
-    })
+      });
+    });
     return completePromise;
-
   },
-  mount: function mount(drivePath, localPath = null, username = null, password = null) {
+  mount: function mount(
+    drivePath,
+    localPath = null,
+    username = null,
+    password = null
+  ) {
     /* parametetr localPath is unused -> allow to have the same signature */
     let completePromise = new Promise((resolve, reject) => {
-
       let test = macNetworkDrive.find(drivePath).then(result => {
         if (result !== undefined) {
-          resolve(result)
+          resolve(result);
         } else {
-          let connectPath = ""
-          let serverPath = drivePath
+          let connectPath = "";
+          let serverPath = drivePath;
 
           if (username != null && password != null) {
-            username = username.replace("\\", "\\\\")
-            connectPath = `${username}:${password}@`
+            username = username.replace("\\", "\\\\");
+            connectPath = `${username}:${password}@`;
           }
 
-          serverPath = serverPath.replace("\\\\", "")
-          serverPath = serverPath.replace("\\", "/")
+          serverPath = serverPath.replace("\\\\", "");
+          serverPath = serverPath.replace("\\", "/");
 
-          let pathDrive = `smb://${connectPath}${serverPath}`
+          let pathDrive = `smb://${serverPath}`;
           let mountScript = `
             tell application "Finder"
             with timeout of 2 seconds
               try
-                mount volume "${pathDrive}"
+                mount volume "${pathDrive}" as user name "${username}" with password "${password}"
               end try
               end timeout
-            end tell`
+            end tell`;
 
           applescript.execString(mountScript, (err, result) => {
             if (err || result === undefined) {
-              if (err === undefined)
-                reject("Unable to connect")
+              if (err === undefined) reject("Unable to connect");
 
-              reject(err)
+              reject(err);
             }
             macNetworkDrive.find(drivePath).then(result => {
-              resolve(result)
-            })
-          })
+              resolve(result);
+            });
+          });
         }
-      })
-    })
+      });
+    });
     return completePromise;
   },
-  unmount: function unmount() {
-
-  }
-}
-exports.list = macNetworkDrive.list
-exports.find = macNetworkDrive.find
-exports.mount = macNetworkDrive.mount
-exports.unmount = macNetworkDrive.unmount
+  unmount: function unmount() {}
+};
+exports.list = macNetworkDrive.list;
+exports.find = macNetworkDrive.find;
+exports.mount = macNetworkDrive.mount;
+exports.unmount = macNetworkDrive.unmount;
